ba6e98f5915582d3469d1a8eb36906531b690a06
yerimstar <yerimhuh@naver.com>
2021-08-24 00:17:59 +0900

    210824 - 날짜 코드 추가, 칼럼명 수정, json 파일 저장 코드 추가

--- 데이터수집/src/pykrx_test.py
+++ 데이터수집/src/pykrx_test.py
@@ -2,11 +2,15 @@ import json
 from pykrx import stock
 from datetime import datetime
 
+today = str(datetime.now())
+today = today[:4]+today[5:7]+today[8:10]
+
 while(1):
-    df_stock = stock.get_market_ohlcv_by_ticker(date="20210823",market="ALL")
-    df_stock = df_stock.reset_index()
     print(datetime.now())
+    df_stock = stock.get_market_ohlcv_by_ticker(date=today,market="ALL")
+    df_stock = df_stock.reset_index()
     df_stock.columns = ['code','open','high','low','close','volume','amount','rate']
     js_stock = df_stock.to_json(orient='records',force_ascii=False)
     with open("./stock_data.json", "w",encoding='utf-8') as json_file:
         json.dump(js_stock, json_file,ensure_ascii = False)
+
