94254b1050536030fccb41ee644f83baf2e37887
yerimstar <yerimhuh@naver.com>
2021-08-23 12:23:12 +0900

    210823

--- 데이터수집/src/data_month.py
+++ 데이터수집/src/data_month.py
@@ -8,8 +8,7 @@ code = '005930'
 today = datetime.date.today()
 month_before = datetime.datetime.now()-relativedelta(months=1)
 month_before = month_before.date()
-# print(today)
-# print(month_before)
+
 data = fdr.DataReader(code,month_before,today) # 종목코드 입력
 data.to_csv('../data/data_month_' + code + '.csv')
 csv_data = pd.read_csv('../data/data_month_' + code + '.csv')
