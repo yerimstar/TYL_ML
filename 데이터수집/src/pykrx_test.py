d7418dc4c5b30443c0cdceb0407c626735f93555
yerimstar <yerimhuh@naver.com>
2021-08-24 00:10:13 +0900

    210824 - 칼럼명 변경, json파일 저장 기능 추가

--- 데이터수집/src/pykrx_test.py
+++ 데이터수집/src/pykrx_test.py
@@ -1,10 +1,12 @@
+import json
 from pykrx import stock
 from datetime import datetime
 
 while(1):
     df_stock = stock.get_market_ohlcv_by_ticker(date="20210823",market="ALL")
     df_stock = df_stock.reset_index()
-    print(df_stock)
     print(datetime.now())
+    df_stock.columns = ['code','open','high','low','close','volume','amount','rate']
     js_stock = df_stock.to_json(orient='records',force_ascii=False)
-    print(js_stock)
+    with open("./stock_data.json", "w",encoding='utf-8') as json_file:
+        json.dump(js_stock, json_file,ensure_ascii = False)
