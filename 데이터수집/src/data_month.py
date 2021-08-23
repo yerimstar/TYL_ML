9aa34d8877bd99cf0c8f369c178072a434182834
yerimstar <yerimhuh@naver.com>
2021-08-23 12:36:03 +0900

    210823

--- 데이터수집/src/data_month.py
+++ 데이터수집/src/data_month.py
@@ -4,7 +4,6 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 code = '005930'
-
 today = datetime.date.today()
 month_before = datetime.datetime.now()-relativedelta(months=1)
 month_before = month_before.date()
