542fff3aaec3944ec2d92d0c2a98e6a15e643e33
yerimstar <yerimhuh@naver.com>
2021-08-24 15:13:32 +0900

    210824 - 시간 체크 코드 추가

--- 데이터수집/src/pykrx_test.py
+++ 데이터수집/src/pykrx_test.py
@@ -2,14 +2,27 @@ import json
 from pykrx import stock
 from datetime import datetime
 
-today = str(datetime.now())
-today = today[:4]+today[5:7]+today[8:10]
-
-while(1):
-    print(datetime.now())
+def get_ohlcv(today):
     df_stock = stock.get_market_ohlcv_by_ticker(date=today,market="ALL")
     df_stock = df_stock.reset_index()
     df_stock.columns = ['code','open','high','low','close','volume','amount','rate']
     js_stock = df_stock.to_json(orient='records',force_ascii=False)
-    with open("../data/stock_data.json", "w", encoding='utf-8') as json_file:
-        json.dump(js_stock, json_file,ensure_ascii = False)
+    js_file = open("../data/stock_data.json", "w", encoding='utf-8')
+    json.dump(js_stock,js_file,ensure_ascii = False)
+    js_file.close()
+
+def fmain():
+    now = str(datetime.now())
+    today = now[:4] + now[5:7] + now[8:10]
+    hour = int(now[11:13])
+    minute = int(now[14:16])
+
+    while (9 <= hour <= 15):
+        now = str(datetime.now())
+        hour = int(now[11:13])
+        minute = int(now[14:16])
+        print(hour, minute)
+        if hour == 15 and minute == 30:
+            break
+        get_ohlcv(today)
+fmain()
